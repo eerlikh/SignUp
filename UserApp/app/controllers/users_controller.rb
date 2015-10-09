0748acc62233558b74e622f5a6a2b677b1153bb9
Eduard Erlikh <eerlikh7@gmail.com>
2015-10-09 03:55:02 -0400

    setting up sign up app

---
+++ UserApp/app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def create
+    user = User.create(user_params)
+    redirect_to users_path(user)
+  end
+
+  def new
+    @user = User.new()
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to users_path(user)
+  end
+
+  def destroy
+    User.destroy(params[:id])
+    redirect_to users_path(user)
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+end
